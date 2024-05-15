4ea190176fe11a9378de448f7a32f5d1e78e14ec
Jose IV <ivjosesantos@gmail.com>
2024-05-15 17:57:58 +0800

    Added tailwind for styling

--- src/componentOne.js
+++ src/componentOne.js
@@ -3,40 +3,55 @@ import data from "./data/componentOne.json";
 export function componentOne() {
 
   document.querySelector("#id-component-one").innerHTML = `
-        <section>
-            <div>
-                ${data.images.map(image => `<div>
-                        <a href="#" id=${`id-component-one-${image.id}`}>
-                            <img src=${`img/component-01/${image.img.md}`} alt="Elva dressed as a fairy" />
-                        </a>
-                    </div>
-                `).join('')}
-            </div>
-            <div>
-                <div>
-                    <h2>${data.title}</h2>
-                    <p>${data.descriptions}</p>
-                </div>
-                <div>
-                    <h2>${data.notes.title}</h2>
-                    <p>${data.notes.descriptions}</p>
-                </div>
-            </div>
+        <section class="max-w-7xl mx-auto pt-24  md:pt-48 px-8">
+            <div class="grid rid-cols-1 lg:grid-cols-3 lg:auto-cols-max  gap-5">
+                ${data.images.map((image, index) => {
+                    if (index === 0)
+                        return `<div>
+                                <a href="#" id=${`id-component-one-${image.id}`}>
+                                    <img  class="w-full" src=${`img/component-01/${image.img.md}`} alt="Elva dressed as a fairy" />
+                                </a>
+                            </div>
+                        `
+                }).join('')}
 
-            <dialog id="id-dialog">
+                <div class="grid grid-rows-2 gap-5">
+                    ${data.images.map((image, index) => {
+                        if (index !== 0)
+                            return `<div class=" ">
+                                    <a href="#" id=${`id-component-one-${image.id}`}>
+                                        <img class="w-full" src=${`img/component-01/${image.img.md}`} alt="Elva dressed as a fairy" />
+                                    </a>
+                                </div>
+                            `
+                    }).join('')}
+                </div>
                 <div>
-                    <button type="submit" aria-label="Close dialog" id="id-cancel-modal">
-                        <svg viewBox="0 0 352 512" width="100" title="times">
+                    <div class="pb-10 text-white" >
+                        <h2 class="text-2xl ">${data.title}</h2>
+                        <hr class="h-px my-4 bg-gray-200 border-1 dark:bg-gray-700">
+                        <p class="text-xl pt-4 font-light ">${data.descriptions}</p>
+                    </div>
+                    <div>
+                        <h3 class="text-[#CD1430] font-bold mb-4">${data.notes.title}</h3>
+                        <p class="text-xl text-white" font-bold">${data.notes.descriptions}</p>
+                    </div>
+                </div>
+            </div>
+            
+    
+           <dialog id="id-dialog" class="bg-white p-2">
+                <div class="text-right">
+                    <button  type="submit" aria-label="Close dialog" id="id-cancel-modal">
+                        <svg class="w-8 h-8" viewBox="0 0 352 512" width="100" title="times">
                             <path d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z" />
                         </svg>
                     </button>
                     
                 </div>
                 <div>
-                    <img id="id-img-dialog" src="" alt="Elva dressed as a fairy" />
-                </div>
-           
-                 
+                    <img class="w-full"  id="id-img-dialog" src="" alt="Elva dressed as a fairy" />
+                </div>      
             </dialog>
         </section>
     `;
--- src/componentTwo.js
+++ src/componentTwo.js
@@ -2,21 +2,21 @@ import data from "./data/componentTwo.json";
 
 export function componentTwo() {
   document.querySelector("#id-component-two").innerHTML = `
-        <section>
-            <h2>${data.title}</h2>
-            <div>
-                ${data.images.map(image => `<div>
+        <section  class="max-w-7xl mx-auto py-24 md:py-48 px-8 text-white">
+            <h3 class="text-center font-light text-5xl py-16">${data.title}</h3>
+            <div class="grid grid-rows-1 md:grid-cols-3 gap-5">
+                ${data.images.map(image => `<div class="pb-6">
                         <a href="#" id=${`id-component-two-${image.id}`}>
-                            <img src=${`img/component-02/${image.img.md}`} alt="Elva dressed as a fairy" />
+                            <img class="border-b-[5px] border-[#CD1430]"  src=${`img/component-02/${image.img.md}`} alt="Elva dressed as a fairy" />
                         </a>
-                        <div>
-                            <h2>${image.title}</h2>
-                            <p>${image.descriptions}</p>
+                        <div class="mt-7">
+                            <h4 class="text-lg font-bold mb-2">${image.title}</h4>
+                            <p class="text-base font-light">${image.descriptions}</p>
                         </div>
-                        <div>
-                        <a href="#" id=${`id-component-two-readme-${image.id}`}>
-                            READ ME
-                        </a>
+                        <div  class="mt-7">
+                            <a href="#" class="text-base py-2 px-1 font-bold border-b-2 border-[#CD1430]" id=${`id-component-two-readme-${image.id}`}>
+                                READ ME
+                            </a>
                         </div>
                     </div>
                 `).join('')}
